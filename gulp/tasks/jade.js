ad19a777e6d7fa64e79f1c35bee848dfe6a7d977
fernandoGuisso <fguisso@gmail.com>
2016-03-23 12:49:14 -0300

    initial commit

---
+++ gulp/tasks/jade.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Necessary Plugins
+var gulp     = require('gulp')
+    ,plumber = require('gulp-plumber')
+    ,paths   = require('../paths')
+    ,fs      = require('fs')
+    ,glob    = require('gulp-jade-globbing')
+    ,jade    = require('gulp-jade');
+
+// Call Jade to compile Templates
+module.exports = gulp.task('jade', function () {
+    console.log(paths.source.files.jade);
+  return gulp.src(paths.source.files.jade)
+    .pipe(plumber())
+    .pipe(glob())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(paths.build.html))
+});
